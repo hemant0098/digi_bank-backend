103960cee5b4498619963160a71fd62075a83374
hemant0098 <hemantdevgan.0098@gmail.com>
2025-05-06 11:11:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const cors = require('cors');
 
 
 const corsOptions = {
-    origin: ['https://digi-bank-frontend.vercel.app', 'http://localhost:3000', 'https://digi-bank-frontend-git-main-hemant-devgans-projects.vercel.app'], // Allow only frontend to access
+    origin: ['https://digi-bank-frontend.vercel.app', 'https://digi-bank-frontend-git-main-hemant-devgans-projects.vercel.app'], // Allow only frontend to access
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow specific HTTP methods
     credentials: true,  // Allow credentials (cookies, Authorization tokens)
 };
 app.options(cors(corsOptions));
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
